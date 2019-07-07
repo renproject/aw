518ca815f92503b39dcdc0062fa60495caa97b58
loongy <loong@renproject.io>
2019-07-08 02:45:14 +1000

    aw: re-export protocol types

--- aw.go
+++ aw.go
@@ -1,4 +1,20 @@
 package aw
 
-type Peer interface {
-}
+import (
+	"github.com/renproject/aw/protocol"
+)
+
+type (
+	Message         = protocol.Message
+	MessageSender   = protocol.MessageSender
+	MessageReceiver = protocol.MessageReceiver
+
+	Event         = protocol.Event
+	EventSender   = protocol.EventSender
+	EventReceiver = protocol.EventReceiver
+
+	PeerID        = protocol.PeerID
+	PeerIDs       = protocol.PeerIDs
+	PeerAddress   = protocol.PeerAddress
+	PeerAddresses = protocol.PeerAddresses
+)
