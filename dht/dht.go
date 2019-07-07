762d9a6601412d9cd8a0baa0949f7850d03ba917
loongy <loong@renproject.io>
2019-07-08 02:14:49 +1000

    dht: def all interface methods

--- dht/dht.go
+++ dht/dht.go
@@ -3,6 +3,8 @@ package dht
 import "github.com/renproject/aw/protocol"
 
 type DHT interface {
+	AddPeerAddress(protocol.PeerAddress) error
+	RemovePeerAddress(protocol.PeerAddress) error
 	PeerAddress(protocol.PeerID) (protocol.PeerAddress, error)
 	PeerAddresses() (protocol.PeerAddresses, error)
 }
