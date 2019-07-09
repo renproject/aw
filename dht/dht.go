0449fe7b35f6de47369a5257222bd59daddfa39d
loongy <loong@renproject.io>
2019-07-09 12:39:14 +0800

    dht: cache all peer addrs in mem

--- dht/dht.go
+++ dht/dht.go
@@ -2,59 +2,70 @@ package dht
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/renproject/aw/protocol"
 	"github.com/renproject/kv"
 )
 
+// A DHT is a distributed hash table. It is used for storing peer addresses. A
+// DHT is not required to be persistent and will often purge stale peer
+// addresses.
 type DHT interface {
 	Me() protocol.PeerAddress
-	AddPeerAddress(protocol.PeerAddress) error
-	AddPeerAddresses(peerAddrs protocol.PeerAddresses) error
-	RemovePeerAddress(protocol.PeerID) error
+	NumPeers() (int, error)
 	PeerAddress(protocol.PeerID) (protocol.PeerAddress, error)
 	PeerAddresses() (protocol.PeerAddresses, error)
-	PeerCount() (int, error)
+
+	AddPeerAddress(protocol.PeerAddress) error
+	AddPeerAddresses(protocol.PeerAddresses) error
+	RemovePeerAddress(protocol.PeerID) error
 }
 
 type dht struct {
 	me    protocol.PeerAddress
 	codec protocol.PeerAddressCodec
 	store kv.Iterable
+
+	inMemCacheMu *sync.RWMutex
+	inMemCache   map[string]protocol.PeerAddress
 }
 
-func New(me protocol.PeerAddress, codec protocol.PeerAddressCodec, store kv.Iterable) DHT {
-	return &dht{
+// New DHT that stores peer addresses in the given store. It will cache all peer
+// addresses in memory for fast access. It is safe for concurrent use,
+// regardless of the underlying store.
+func New(me protocol.PeerAddress, codec protocol.PeerAddressCodec, store kv.Iterable) (DHT, error) {
+	dht := &dht{
 		me:    me,
 		codec: codec,
 		store: store,
+
+		inMemCacheMu: new(sync.RWMutex),
+		inMemCache:   map[string]protocol.PeerAddress{},
 	}
+	if err := dht.fillInMemCache(); err != nil {
+		return nil, err
+	}
+	return dht, nil
 }
 
 func (dht *dht) Me() protocol.PeerAddress {
 	return dht.me
 }
 
-func (dht *dht) PeerCount() (int, error) {
-	return dht.store.Size()
+func (dht *dht) NumPeers() (int, error) {
+	dht.inMemCacheMu.RLock()
+	defer dht.inMemCacheMu.RUnlock()
+
+	return len(dht.inMemCache), nil
 }
 
 func (dht *dht) PeerAddresses() (protocol.PeerAddresses, error) {
-	peerAddrs := []protocol.PeerAddress{}
-	iterator, err := dht.store.Iterator()
-	if err != nil {
-		return nil, err
-	}
+	dht.inMemCacheMu.RLock()
+	defer dht.inMemCacheMu.RUnlock()
 
-	for iterator.Next() {
-		peer := []byte{}
-		if err := iterator.Value(&peer); err != nil {
-			return nil, err
-		}
-		peerAddr, err := dht.codec.Decode(peer)
-		if err != nil {
-			return nil, err
-		}
+	peerAddrs := make(protocol.PeerAddresses, 0, len(dht.inMemCache))
+	for _, peerAddr := range dht.inMemCache {
 		peerAddrs = append(peerAddrs, peerAddr)
 	}
 
@@ -62,16 +73,18 @@ func (dht *dht) PeerAddresses() (protocol.PeerAddresses, error) {
 }
 
 func (dht *dht) AddPeerAddress(peerAddr protocol.PeerAddress) error {
-	data, err := dht.codec.Encode(peerAddr)
-	if err != nil {
-		return fmt.Errorf("failed to stringify the given multi address: %v", err)
-	}
-	return dht.store.Insert(peerAddr.PeerID().String(), data)
+	dht.inMemCacheMu.Lock()
+	defer dht.inMemCacheMu.Unlock()
+
+	return dht.addPeerAddressWithoutLock(peerAddr)
 }
 
 func (dht *dht) AddPeerAddresses(peerAddrs protocol.PeerAddresses) error {
-	for _, addr := range peerAddrs {
-		if err := dht.AddPeerAddress(addr); err != nil {
+	dht.inMemCacheMu.Lock()
+	defer dht.inMemCacheMu.Unlock()
+
+	for _, peerAddr := range peerAddrs {
+		if err := dht.addPeerAddressWithoutLock(peerAddr); err != nil {
 			return err
 		}
 	}
@@ -79,13 +92,51 @@ func (dht *dht) AddPeerAddresses(peerAddrs protocol.PeerAddresses) error {
 }
 
 func (dht *dht) PeerAddress(id protocol.PeerID) (protocol.PeerAddress, error) {
-	peer := []byte{}
-	if err := dht.store.Get(id.String(), peer); err != nil {
-		return nil, err
+	dht.inMemCacheMu.RLock()
+	defer dht.inMemCacheMu.RUnlock()
+
+	peerAddr, ok := dht.inMemCache[id.String()]
+	if !ok {
+		return nil, fmt.Errorf("peer=%v not found", id)
 	}
-	return dht.codec.Decode(peer)
+	return peerAddr, nil
 }
 
 func (dht *dht) RemovePeerAddress(id protocol.PeerID) error {
-	return dht.store.Delete(id.String())
+	dht.inMemCacheMu.Lock()
+	defer dht.inMemCacheMu.Unlock()
+
+	if err := dht.store.Delete(id.String()); err != nil {
+		return fmt.Errorf("error deleting peer=%v from dht: %v", id, err)
+	}
+	delete(dht.inMemCache, id.String())
+
+	return nil
+}
+
+func (dht *dht) addPeerAddressWithoutLock(peerAddr protocol.PeerAddress) error {
+	data, err := dht.codec.Encode(peerAddr)
+	if err != nil {
+		return fmt.Errorf("error encoding peer address=%v: %v", peerAddr, err)
+	}
+	if err := dht.store.Insert(peerAddr.PeerID().String(), data); err != nil {
+		return fmt.Errorf("error inserting peer address=%v into dht: %v", peerAddr, err)
+	}
+	dht.inMemCache[peerAddr.PeerID().String()] = peerAddr
+	return nil
+}
+
+func (dht *dht) fillInMemCache() error {
+	iter, err := dht.store.Iterator()
+	if err != nil {
+		return fmt.Errorf("error initialising dht iterator: %v", err)
+	}
+	for iter.Next() {
+		var peerAddr protocol.PeerAddress
+		if err := iter.Value(&peerAddr); err != nil {
+			return fmt.Errorf("error scanning dht iterator: %v", err)
+		}
+		dht.inMemCache[peerAddr.PeerID().String()] = peerAddr
+	}
+	return nil
 }
