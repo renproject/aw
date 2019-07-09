2fbcc12841a3b68e7df95e4d118626d7b4b41b90
susruth <susruth@republicprotocol.com>
2019-07-09 11:15:05 +1000

    tcp: updated to use messages with random lengths

--- tcp/tcp_test.go
+++ tcp/tcp_test.go
@@ -3,6 +3,8 @@ package tcp_test
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"fmt"
 	"net"
 	"reflect"
 	"testing/quick"
@@ -18,8 +20,9 @@ import (
 
 var _ = Describe("Tcp", func() {
 	startServer := func(ctx context.Context, bind string, sender protocol.MessageSender) {
+		logger := logrus.StandardLogger()
 		err := NewServer(ServerOptions{
-			Logger:  logrus.StandardLogger(),
+			Logger:  logger,
 			Timeout: time.Minute,
 		}, sender).Listen(ctx, bind)
 		if err != nil {
@@ -28,9 +31,10 @@ var _ = Describe("Tcp", func() {
 	}
 
 	startClient := func(ctx context.Context, receiver protocol.MessageReceiver) {
+		logger := logrus.StandardLogger()
 		NewClient(
 			NewClientConns(ClientOptions{
-				Logger:         logrus.StandardLogger(),
+				Logger:         logger,
 				Timeout:        time.Minute,
 				MaxConnections: 10,
 			}),
@@ -39,32 +43,41 @@ var _ = Describe("Tcp", func() {
 	}
 
 	Context("when sending a valid message", func() {
-		It("should work", func() {
+		It("should successfully send and receive", func() {
 			sender := make(chan protocol.MessageOnTheWire, 10)
 			receiver := make(chan protocol.MessageOnTheWire, 10)
-			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
-			go startServer(ctx, "0.0.0.0:49673", sender)
+			// start TCP server and client
+			go startServer(ctx, fmt.Sprintf("127.0.0.1:47326"), sender)
 			go startClient(ctx, receiver)
-			addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:49673")
+
+			addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:47326"))
 			Expect(err).Should(BeNil())
 
-			iteration := func(val uint16, body [16]byte) bool {
+			iteration := func(val uint32) bool {
+				// create message body of random length between 2 Bytes and
+				// 4 Mega Bytes
+				body := make([]byte, int(val/1000)+2)
+				rand.Read(body)
+
 				variant := protocol.MessageVariant(val%5 + 1)
 				receiver <- protocol.MessageOnTheWire{
 					To:      addr,
-					Message: protocol.NewMessage(protocol.V1, variant, body[:]),
+					Message: protocol.NewMessage(protocol.V1, variant, body),
 				}
 				msg := <-sender
 
-				return (bytes.Compare(msg.Message.Body, body[:]) == 0 &&
+				return (bytes.Compare(msg.Message.Body, body) == 0 &&
 					reflect.DeepEqual(msg.Message.Variant, variant) &&
 					reflect.DeepEqual(msg.Message.Version, protocol.V1) &&
-					msg.Message.Length == 48)
+					msg.Message.Length == protocol.MessageLength(len(body)+32))
 			}
 
-			Expect(quick.Check(iteration, nil)).Should(BeNil())
+			Expect(quick.Check(iteration, &quick.Config{
+				MaxCount: 10,
+			})).Should(BeNil())
 		})
 	})
 })
