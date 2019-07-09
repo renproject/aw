426b731472749a85e71b4dddca72336ad0654d7b
susruth <susruth@republicprotocol.com>
2019-07-09 16:29:54 +1000

    Add test suite file

--- aw_suite_test.go
+++ aw_suite_test.go
@@ -1,2 +1,13 @@
 package aw_test
 
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestAw(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Aw Suite")
+}
