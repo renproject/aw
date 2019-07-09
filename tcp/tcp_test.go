cb3d618f3ebe247f44f5e210ae909b1bf57a9b49
loongy <loong@renproject.io>
2019-07-09 11:07:44 +0800

    tcp: comment test

--- tcp/tcp_test.go
+++ tcp/tcp_test.go
@@ -10,12 +10,12 @@ import (
 	"testing/quick"
 	"time"
 
+	"github.com/renproject/aw/protocol"
+	"github.com/sirupsen/logrus"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/renproject/aw/tcp"
-
-	"github.com/renproject/aw/protocol"
-	"github.com/sirupsen/logrus"
 )
 
 var _ = Describe("Tcp", func() {
@@ -44,35 +44,46 @@ var _ = Describe("Tcp", func() {
 
 	Context("when sending a valid message", func() {
 		It("should successfully send and receive", func() {
-			sender := make(chan protocol.MessageOnTheWire, 10)
-			receiver := make(chan protocol.MessageOnTheWire, 10)
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
+			fromServer := make(chan protocol.MessageOnTheWire, 10)
+			toClient := make(chan protocol.MessageOnTheWire, 10)
+
 			// start TCP server and client
-			go startServer(ctx, fmt.Sprintf("127.0.0.1:47326"), sender)
-			go startClient(ctx, receiver)
+			go startServer(ctx, fmt.Sprintf("127.0.0.1:47326"), fromServer)
+			go startClient(ctx, toClient)
 
 			addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:47326"))
 			Expect(err).Should(BeNil())
 
-			iteration := func(val uint32) bool {
-				// create message body of random length between 2 Bytes and
-				// 4 Mega Bytes
-				body := make([]byte, int(val/1000)+2)
-				rand.Read(body)
+			iteration := func(v int, numBytes int) bool {
+				// Set variant and body size to an appropriate range
+				v = v%5 + 1
+				numBytes = numBytes % 1000000 // 0 bytes to 1 megabyte
 
-				variant := protocol.MessageVariant(val%5 + 1)
-				receiver <- protocol.MessageOnTheWire{
+				// Generate random variant
+				variant := protocol.MessageVariant(v)
+				body := make([]byte, numBytes)
+				if len(body) > 0 {
+					n, err := rand.Read(body)
+					Expect(n).To(Equal(numBytes))
+					Expect(err).ToNot(HaveOccurred())
+				}
+
+				// Send a message to the server using the client and read from
+				// message received by the server
+				toClient <- protocol.MessageOnTheWire{
 					To:      addr,
 					Message: protocol.NewMessage(protocol.V1, variant, body),
 				}
-				msg := <-sender
+				messageWire := <-fromServer
 
-				return (bytes.Compare(msg.Message.Body, body) == 0 &&
-					reflect.DeepEqual(msg.Message.Variant, variant) &&
-					reflect.DeepEqual(msg.Message.Version, protocol.V1) &&
-					msg.Message.Length == protocol.MessageLength(len(body)+32))
+				// Check that the message sent equals the message received
+				return (messageWire.Message.Length == protocol.MessageLength(len(body)+32) &&
+					reflect.DeepEqual(messageWire.Message.Version, protocol.V1) &&
+					reflect.DeepEqual(messageWire.Message.Variant, variant) &&
+					bytes.Compare(messageWire.Message.Body, body) == 0)
 			}
 
 			Expect(quick.Check(iteration, &quick.Config{
