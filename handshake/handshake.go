010f5e65dc349fed2421f433f60b9f94730f04df
susruth <susruth@republicprotocol.com>
2019-07-26 12:02:07 +1000

    handshake: implemented 2way async handshake

--- handshake/handshake.go
+++ handshake/handshake.go
@@ -1,55 +1,73 @@
 package handshake
 
 import (
+	"bytes"
 	"context"
-	"crypto/rsa"
+	"encoding/binary"
+	"fmt"
 	"io"
+	"time"
+
+	"golang.org/x/crypto/sha3"
 
 	"github.com/renproject/aw/protocol"
 )
 
 type HandShaker interface {
-	OnConnect(ctx context.Context, rw io.ReadWriter) error
-	OnReceive(ctx context.Context, rw io.ReadWriter) error
-	OnChallengeAccept(ctx context.Context, rw io.ReadWriter) error
-	OnChallengeResponse(ctx context.Context, rw io.ReadWriter) error
+	SendHandShakeMessage(ctx context.Context, rw io.ReadWriter, myAddress []byte) error
+	ValidateHandShakeMessage(ctx context.Context, rw io.ReadWriter, remoteAddress []byte) error
 }
 
 type handShaker struct {
-	rsaPrivKey     *rsa.PrivateKey
-	signerVerifier protocol.SignerVerifier
+	signVerifier protocol.SignVerifier
 }
 
-func NewHandShaker(signerVerifier protocol.SignerVerifier) HandShaker {
-	return &handShaker{signerVerifier: signerVerifier}
+func NewHandShaker(signVerifier protocol.SignVerifier) HandShaker {
+	return &handShaker{signVerifier: signVerifier}
 }
 
-func (sh *handShaker) OnConnect(ctx context.Context, rw io.ReadWriter) error {
-	// privKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// sh.rsaPrivKey = privKey
-	// pubKeyBytes := serializePublicKey(privKey.PublicKey)
-	// hash := sha3.Sum256(pubKeyBytes)
-	// sig, err := sh.signerVerifier.Sign(hash[:])
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// return NewMessage(append(pubKeyBytes, sig...))
+func (hs *handShaker) SendHandShakeMessage(ctx context.Context, rw io.ReadWriter, myAddress []byte) error {
+	timeStamp := [8]byte{}
+	_ = binary.PutVarint(timeStamp[:], time.Now().Unix())
+	data := append(timeStamp[:], myAddress...)
+	hash := sha3.Sum256(data)
+	sig, err := hs.signVerifier.Sign(hash[:])
+	if err != nil {
+		return err
+	}
+	if err := binary.Write(rw, binary.LittleEndian, uint64(len(data)+65)); err != nil {
+		return err
+	}
+	if _, err := rw.Write(append(data, sig...)); err != nil {
+		return err
+	}
 	return nil
 }
 
-func (sh *handShaker) OnReceive(ctx context.Context, rw io.ReadWriter) error {
-	return nil
-}
-func (sh *handShaker) OnChallengeAccept(ctx context.Context, rw io.ReadWriter) error {
-	return nil
-}
-func (sh *handShaker) OnChallengeResponse(ctx context.Context, rw io.ReadWriter) error {
-	return nil
-}
-
-func serializePublicKey(pubKey rsa.PublicKey) []byte {
+func (hs *handShaker) ValidateHandShakeMessage(ctx context.Context, rw io.ReadWriter, remoteAddress []byte) error {
+	var msgLen uint64
+	if err := binary.Read(rw, binary.LittleEndian, &msgLen); err != nil {
+		return err
+	}
+	msg := make([]byte, msgLen)
+	if _, err := rw.Read(msg); err != nil {
+		return err
+	}
+	data := msg[:msgLen-65]
+	sig := msg[msgLen-65:]
+	hash := sha3.Sum256(data)
+	if err := hs.signVerifier.Verify(hash[:], sig); err != nil {
+		return err
+	}
+	timeStamp, err := binary.ReadVarint(bytes.NewBuffer(data[:8]))
+	if err != nil {
+		return err
+	}
+	if time.Since(time.Unix(timeStamp, 0)) > time.Minute {
+		return fmt.Errorf("signature expired")
+	}
+	if bytes.Compare(data[8:], remoteAddress) != 0 {
+		return fmt.Errorf("possible mim attack")
+	}
 	return nil
 }
