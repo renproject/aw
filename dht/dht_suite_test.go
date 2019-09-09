5accfc8c7cad55428f9416e4efa521ba7cee7d3d
susruth <susruth@renproject.io>
2019-09-09 17:39:57 +1000

    dht: add dht test suite

--- dht/dht_suite_test.go
+++ dht/dht_suite_test.go
@@ -1,2 +1,13 @@
 package dht_test
 
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestDht(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Dht Suite")
+}
