514ff9f8dfe0ccbd4fe103ced704eb88fea1c2ae
Loong <loong@republicprotocol.com>
2019-10-29 15:58:06 +0800

    handshake: write test cases

--- handshake/handshake_test.go
+++ handshake/handshake_test.go
@@ -1,66 +1,120 @@
 package handshake_test
 
 import (
-	"context"
-	"net"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/renproject/aw/handshake"
-
-	"github.com/renproject/aw/handshake/session"
-	"github.com/renproject/aw/testutil"
-	"github.com/renproject/phi"
 )
 
 var _ = Describe("Handshaker", func() {
-	Context("when both parties are authenticated", func() {
-		It("should sucessfully perform the handshake with gcm session creator", func() {
-			clientSV := testutil.NewMockSignVerifier()
-			serverSV := testutil.NewMockSignVerifier(clientSV.ID())
-			clientSV.Whitelist(serverSV.ID())
+	Context("when using gcm handshakes", func() {
+		Context("when the client is honest", func() {
+			Context("when the server is honest", func() {
+				Context("when handshaking", func() {
+					It("should authenticate the client and server", func() {
+						Expect(true).To(BeFalse())
+					})
 
-			sessionCreator := session.NewGCMSessionCreator()
-			client := New(clientSV, sessionCreator)
-			server := New(serverSV, sessionCreator)
+					It("should cipher and decipher messages", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+			})
 
-			c2s, s2c := net.Pipe()
-			phi.ParBegin(
-				func() {
-					defer GinkgoRecover()
-					_, err := client.Handshake(context.Background(), c2s)
-					Expect(err).Should(BeNil())
-				},
-				func() {
-					defer GinkgoRecover()
-					_, err := server.AcceptHandshake(context.Background(), s2c)
-					Expect(err).Should(BeNil())
-				},
-			)
+			Context("when the server is dishonest", func() {
+				Context("when the server sends a malformed rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends an incorrectly encrypted session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends an invalid session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends an invalid signature for the server session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends an invalid signature for the server rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends a valid signaturefor the server rsa.PublicKey that does not match the signature for the server session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends an invalid signature for the client session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the server sends a valid signature for the client session key that does not match the signature for the server session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+			})
 		})
-	})
 
-	It("should sucessfully perform the handshake with nop session creator", func() {
-		clientSV := testutil.NewMockSignVerifier()
-		serverSV := testutil.NewMockSignVerifier(clientSV.ID())
-		clientSV.Whitelist(serverSV.ID())
+		Context("when the server is honest", func() {
+			Context("when the client is dishonest", func() {
+				Context("when the client sends a malformed rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
 
-		sessionCreator := session.NewNOPSessionCreator()
-		client := New(clientSV, sessionCreator)
-		server := New(serverSV, sessionCreator)
+				Context("when the client sends an invalid signature for their rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
 
-		c2s, s2c := net.Pipe()
-		phi.ParBegin(
-			func() {
-				defer GinkgoRecover()
-				_, err := client.Handshake(context.Background(), c2s)
-				Expect(err).Should(BeNil())
-			},
-			func() {
-				defer GinkgoRecover()
-				_, err := server.AcceptHandshake(context.Background(), s2c)
-				Expect(err).Should(BeNil())
-			},
-		)
+				Context("when the client sends an invalid signature for the server session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the client sends a valid signature for the server session key that does not match the signature for the client rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the client sends an incorrectly encrypted session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the client sends an invalid signature for the client session key", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+
+				Context("when the client sends a valid signature for the client session key that does not match the signature for the client rsa.PublicKey", func() {
+					It("should return an error", func() {
+						Expect(true).To(BeFalse())
+					})
+				})
+			})
+		})
 	})
 })
