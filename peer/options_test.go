f4fe38f4b7bd40cb6dcbf1d21ff9445a23df2352
tok-kkk <yunshi@republicprotocol.com>
2019-11-19 15:51:50 +1100

    add missing back-slack in the circle ci config yaml

--- peer/options_test.go
+++ peer/options_test.go
@@ -3,12 +3,30 @@ package peer_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/renproject/aw/peer"
+	"github.com/sirupsen/logrus"
 )
 
 var _ = Describe("options", func() {
 	Context("initializing options", func() {
+		It("should return an error if not providing a logger", func() {
+			option := Options{}
+			Expect(option.SetZeroToDefault()).To(HaveOccurred())
+		})
+
+		It("should return an error if not providing a self PeerAddress", func() {
+			option := Options{
+				Logger: logrus.StandardLogger(),
+			}
+			Expect(option.SetZeroToDefault()).To(HaveOccurred())
+		})
+
 		It("should set unset filed to default value", func() {
-			Expect(true).Should(BeTrue())
+			option := Options{
+				Logger: logrus.StandardLogger(),
+				Me:     random,
+			}
+			Expect(option.SetZeroToDefault()).To(HaveOccurred())
 		})
 	})
 })
