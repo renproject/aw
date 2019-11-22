7bb5d93b71c3a3506d86be8b498a91e66ca8739f
tok-kkk <yunshi@republicprotocol.com>
2019-11-22 15:46:22 +1100

    organize code in handshake file

--- handshake/handshake.go
+++ handshake/handshake.go
@@ -1,37 +1,12 @@
-//  1.(Client) Generate a new RSA key ClientRSA.
-//  2.(Client) Convert the public key of ClientRSA to bytes and write to server.
-//  3.(Client) Sign the hash of RSA public key bytes and write to server.
-//  4.(Server) Read signature from client and verify the signature.
-//  5.(Server) Generate a new session key ServerSessionKey
-//  6.(Server) Encrypt the ServerSessionKey with ClientRSA and write to client
-//  7.(Server) Sign the hash of ServerSessionKey and write to client.
-//  8.(Client) Read and decrypt ServerSessionKey and verify the signature.
-//  9.(Client) Sign the hash of ServerSessionKey and write to server.
-// 10.(Server) Read signature and verify it to confirm client has received the ServerSessionKey.
-// 11.(Server) Generate a new RSA key ServerRSA.
-// 12.(Server) Convert the public key of ServerRSA to bytes and write to client.
-// 13.(Server) Sign the hash of public key of ServerRSA and write to client.
-// 14.(Client) Read signature from server and verify the signature.
-// 15.(Client) Generate a new session key ClientSessionKey
-// 16.(Client) Encrypt the ClientSessionKey with ServerRSA and write to server
-// 17.(Client) Sign the hash of ClientSessionKey and write to server.
-// 18.(Server) Read and decrypt ClientSessionKey and verify the signature.
-// 19.(Server) Sign the hash of ClientSessionKey and write to client.
-// 20.(Client) Read signature and verify it to confirm server has received the ClientSessionKey.
-// 21.(Client & Server) Xor the ClientSessionKey and ServerSessionKey to get the actual session key
-
 package handshake
 
 import (
-	"bytes"
 	"context"
 	"crypto/ecdsa"
 	"crypto/rand"
-	"crypto/rsa"
 	"encoding/binary"
 	"fmt"
 	"io"
-	"math/big"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/ecies"
@@ -63,136 +38,138 @@ func New(signVerifier protocol.SignVerifier, sessionManager protocol.SessionMana
 	if sessionManager == nil {
 		panic("invariant violation: SessionManager cannot be nil")
 	}
-	return &handshaker{signVerifier: signVerifier, sessionManager: sessionManager}
+	return &handshaker{
+		signVerifier:   signVerifier,
+		sessionManager: sessionManager,
+	}
 }
 
 func (hs *handshaker) Handshake(ctx context.Context, rw io.ReadWriter) (protocol.Session, error) {
+	// 1. Write self ecdsa public key and Signature of it.
 	localPrivateKey, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
 	if err != nil {
 		return nil, fmt.Errorf("error generating new ecdsa key : %v", err)
 	}
+	if err := hs.writePublicKey(rw, localPrivateKey); err != nil {
+		return nil, err
+	}
 
-	// Write self PeerID and Signature of it.
-	localPublicKey := localPrivateKey.PublicKey
+	// 2. Read the remote ECDSA public key and verify the signature.
+	remotePublicKey, remotePeerID, err := hs.readPublicKey(rw)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. Generate a session key and write to server
+	localSessionKey := hs.sessionManager.NewSessionKey()
+	if err := hs.writeEncrypted(rw, localSessionKey, remotePublicKey); err != nil {
+		return nil, err
+	}
+
+	// 4. Read and decrypt the session key from the server.
+	remoteSessionKey, err := hs.readEncrypted(rw, localPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return hs.sessionManager.NewSession(remotePeerID, xorSessionKeys(localSessionKey, remoteSessionKey)), nil
+}
+
+func (hs *handshaker) AcceptHandshake(ctx context.Context, rw io.ReadWriter) (protocol.Session, error) {
+	// 1. Read the remote ECDSA public key and verify the signature.
+	remotePublicKey, remotePeerID, err := hs.readPublicKey(rw)
+	if err != nil {
+		return nil, err
+	}
+
+	// 2. Write self ecdsa public key and Signature of it.
+	localPrivateKey, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("error generating new ecdsa key : %v", err)
+	}
+	if err := hs.writePublicKey(rw, localPrivateKey); err != nil {
+		return nil, err
+	}
+
+	// 3. Read and decrypt the session key from the server.
+	remoteSessionKey, err := hs.readEncrypted(rw, localPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// 4. Generate a session key and write to server
+	localSessionKey := hs.sessionManager.NewSessionKey()
+	if err := hs.writeEncrypted(rw, localSessionKey, remotePublicKey); err != nil {
+		return nil, err
+	}
+	return hs.sessionManager.NewSession(remotePeerID, xorSessionKeys(localSessionKey, remoteSessionKey)), nil
+}
+
+// Write the ecdsa public along with a signature of it through the io.Writer
+func (hs *handshaker) writePublicKey(w io.Writer, key *ecdsa.PrivateKey) error {
+	localPublicKey := key.PublicKey
 	localPublicKeyBytes := crypto.FromECDSAPub(&localPublicKey)
-	if err := write(rw, localPublicKeyBytes); err != nil {
-		return nil, fmt.Errorf("error writing ecdsa.PublicKey to io.Writer: %v", err)
+	if err := write(w, localPublicKeyBytes); err != nil {
+		return fmt.Errorf("error writing ecdsa.PublicKey to io.Writer: %v", err)
 	}
 	pubKeySig, err := hs.signVerifier.Sign(hs.signVerifier.Hash(localPublicKeyBytes))
 	if err != nil {
-		panic(fmt.Errorf("invariant violation: cannot sign ecdsa.publickey: %v", err))
+		return fmt.Errorf("invariant violation: cannot sign ecdsa.publickey: %v", err)
 	}
-	if err := write(rw, pubKeySig); err != nil {
-		return nil, fmt.Errorf("error writing rsa.PublicKey signature to io.Writer: %v", err)
+	if err := write(w, pubKeySig); err != nil {
+		return fmt.Errorf("error writing ecdsa.PublicKey signature to io.Writer: %v", err)
 	}
+	return nil
+}
 
-	// Read the remote ECDSA public key and verify the signature.
-	remotePubKeyBytes, err := read(rw)
+// Unmarshal the read data to an ecdsa.PublicKey and verify the signature.
+func (hs *handshaker) readPublicKey(r io.Reader) (*ecdsa.PublicKey, protocol.PeerID, error) {
+	remotePubKeyBytes, err := read(r)
 	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey from io.Reader: %v", err)
+		return nil, nil, fmt.Errorf("error reading ecdsa.PublicKey from io.Reader: %v", err)
 	}
 	remotePublicKey, err := crypto.UnmarshalPubkey(remotePubKeyBytes)
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling ecdsa PublicKey: %v", err)
+		return nil, nil, fmt.Errorf("error unmarshaling ecdsa PublicKey: %v", err)
 	}
-	remotePubKeySig, err := read(rw)
+	remotePubKeySig, err := read(r)
 	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey signature from io.Reader: %v", err)
+		return nil, nil, fmt.Errorf("error reading ecdsa.PublicKey signature from io.Reader: %v", err)
 	}
 	remotePeerID, err := hs.signVerifier.Verify(hs.signVerifier.Hash(remotePubKeyBytes), remotePubKeySig)
 	if err != nil {
-		return nil, fmt.Errorf("error verifying rsa.PublicKey: %v", err)
+		return nil, nil, fmt.Errorf("error verifying ecdsa.PublicKey: %v", err)
 	}
+	return remotePublicKey, remotePeerID, nil
+}
 
-	// Generate a session key and write to server
-	sessionKey := hs.sessionManager.NewSessionKey()
-	data, err := ecies.Encrypt(rand.Reader, ecies.ImportECDSAPublic(remotePublicKey), sessionKey, nil, nil)
+// encrypt the data with given public key and write the encrypted data through an io.Writer.
+func (hs *handshaker) writeEncrypted(w io.Writer, data []byte, publicKey *ecdsa.PublicKey) error {
+	data, err := ecies.Encrypt(rand.Reader, ecies.ImportECDSAPublic(publicKey), data, nil, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error encrypting session key: %v", err)
+		return fmt.Errorf("error encrypting session key: %v", err)
 	}
-	if err := write(rw, data); err != nil {
-		return nil, fmt.Errorf("error writing rsa.PublicKey signature to io.Writer: %v", err)
+	if err := write(w, data); err != nil {
+		return fmt.Errorf("error writing ecdsa.PublicKey signature to io.Writer: %v", err)
 	}
+	return nil
+}
 
-	// Read and decrypt the session to confirm server receive it.
-	encryptedSessionKey, err := read(rw)
+// read data from the io.Reader and decrypted with the ecdsa.PrivateKey.
+func (hs *handshaker) readEncrypted(r io.Reader, privateKey *ecdsa.PrivateKey) ([]byte, error) {
+	encryptedSessionKey, err := read(r)
 	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey from io.Reader: %v", err)
+		return nil, fmt.Errorf("error reading ecdsa.PublicKey from io.Reader: %v", err)
 	}
-	eciesPrivateKey := ecies.ImportECDSA(localPrivateKey)
+	eciesPrivateKey := ecies.ImportECDSA(privateKey)
 	decryptedSessionKey, err := eciesPrivateKey.Decrypt(encryptedSessionKey, nil, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error decrypting session key from server: %v", err)
 	}
-	if !bytes.Equal(decryptedSessionKey, sessionKey) {
-		return nil, fmt.Errorf("server sends wrong session key back")
-	}
 
-	return hs.sessionManager.NewSession(remotePeerID, sessionKey), nil
+	return decryptedSessionKey, nil
 }
 
-func (hs *handshaker) AcceptHandshake(ctx context.Context, rw io.ReadWriter) (protocol.Session, error) {
-	// Read the remote ECDSA public key and verify the signature.
-	remotePubKeyBytes, err := read(rw)
-	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey from io.Reader: %v", err)
-	}
-	remotePublicKey, err := crypto.UnmarshalPubkey(remotePubKeyBytes)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling remote ecdsa PublicKey: %v", err)
-	}
-	remotePubKeySig, err := read(rw)
-	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey signature from io.Reader: %v", err)
-	}
-	remotePeerID, err := hs.signVerifier.Verify(hs.signVerifier.Hash(remotePubKeyBytes), remotePubKeySig)
-	if err != nil {
-		return nil, fmt.Errorf("error verifying rsa.PublicKey: %v", err)
-	}
-
-	// Write self PeerID and Signature of it.
-	localPrivateKey, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
-	if err != nil {
-		return nil, fmt.Errorf("error generating new ecdsa key : %v", err)
-	}
-	localPublicKey := localPrivateKey.PublicKey
-	localPublicKeyBytes := crypto.FromECDSAPub(&localPublicKey)
-	if err := write(rw, localPublicKeyBytes); err != nil {
-		return nil, fmt.Errorf("error writing rsa.PublicKey to io.Writer: %v", err)
-	}
-	pubKeySig, err := hs.signVerifier.Sign(hs.signVerifier.Hash(localPublicKeyBytes))
-	if err != nil {
-		panic(fmt.Errorf("invariant violation: cannot sign rsa.PublicKey: %v", err))
-	}
-	if err := write(rw, pubKeySig); err != nil {
-		return nil, fmt.Errorf("error writing rsa.PublicKey signature to io.Writer: %v", err)
-	}
-
-	// Read and decrypt session key from the client
-	encryptedSessionKey, err := read(rw)
-	if err != nil {
-		return nil, fmt.Errorf("error reading rsa.PublicKey from io.Reader: %v", err)
-	}
-	eciesPrivateKey := ecies.ImportECDSA(localPrivateKey)
-	sessionKey, err := eciesPrivateKey.Decrypt(encryptedSessionKey, nil, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error decrypting session key from client: %v", err)
-	}
-
-	// Encrypt the session key with client's publick key and write to client
-	data, err := ecies.Encrypt(rand.Reader, ecies.ImportECDSAPublic(remotePublicKey), sessionKey, nil, nil)
-	if err != nil {
-		return nil, fmt.Errorf("error encrypting session key: %v", err)
-	}
-	if err := write(rw, data); err != nil {
-		return nil, fmt.Errorf("error writing rsa.PublicKey signature to io.Writer: %v", err)
-	}
-
-	// fixme : peerID shoud not be nil
-	return hs.sessionManager.NewSession(remotePeerID, sessionKey), nil
-}
-
-// write data to an `io.Writer` with a prefixed length.
 func write(w io.Writer, data []byte) error {
 	if err := binary.Write(w, binary.LittleEndian, uint64(len(data))); err != nil {
 		return fmt.Errorf("error writing data len=%v: %v", len(data), err)
@@ -203,7 +180,6 @@ func write(w io.Writer, data []byte) error {
 	return nil
 }
 
-// read data from an `io.Reader` with a prefixed length.
 func read(r io.Reader) ([]byte, error) {
 	dataLen := uint64(0)
 	if err := binary.Read(r, binary.LittleEndian, &dataLen); err != nil {
@@ -216,71 +192,8 @@ func read(r io.Reader) ([]byte, error) {
 	return data, nil
 }
 
-// writeEncrypted data to an `io.Writer` with a prefixed length.
-func writeEncrypted(w io.Writer, data []byte, pubKey *rsa.PublicKey) error {
-	encryptedData, err := rsa.EncryptPKCS1v15(rand.Reader, pubKey, data)
-	if err != nil {
-		return err
-	}
-	return write(w, encryptedData)
-}
-
-// readEncrypted data from an `io.Reader` with a prefixed length. Returns the
-// data after decryption.
-func readEncrypted(r io.Reader, privKey *rsa.PrivateKey) ([]byte, error) {
-	encryptedData, err := read(r)
-	if err != nil {
-		return nil, err
-	}
-	return rsa.DecryptPKCS1v15(rand.Reader, privKey, encryptedData)
-}
-
-func pubKeyToBytes(pubKey *rsa.PublicKey) ([]byte, error) {
-	buf := new(bytes.Buffer)
-
-	// E
-	if err := binary.Write(buf, binary.LittleEndian, int64(pubKey.E)); err != nil {
-		return buf.Bytes(), fmt.Errorf("error writing rsa.PublicKey: %v", err)
-	}
-
-	// N
-	if err := binary.Write(buf, binary.LittleEndian, uint64(len(pubKey.N.Bytes()))); err != nil {
-		return buf.Bytes(), fmt.Errorf("error writing rsa.PublicKey: %v", err)
-	}
-	if err := binary.Write(buf, binary.LittleEndian, pubKey.N.Bytes()); err != nil {
-		return buf.Bytes(), fmt.Errorf("error writing rsa.PublicKey: %v", err)
-	}
-
-	return buf.Bytes(), nil
-}
-
-func pubKeyFromBytes(data []byte) (rsa.PublicKey, error) {
-	pubKey := rsa.PublicKey{}
-	r := bytes.NewBuffer(data)
-
-	// E
-	e := int64(0)
-	if err := binary.Read(r, binary.LittleEndian, &e); err != nil {
-		return pubKey, fmt.Errorf("error reading rsa.PublicKey: %v", err)
-	}
-	pubKey.E = int(e)
-
-	// N
-	pubKeyLen := uint64(0)
-	if err := binary.Read(r, binary.LittleEndian, &pubKeyLen); err != nil {
-		return pubKey, fmt.Errorf("error reading rsa.PublicKey: %v", err)
-	}
-	pubKeyData := make([]byte, pubKeyLen)
-	if err := binary.Read(r, binary.LittleEndian, &pubKeyData); err != nil {
-		return pubKey, fmt.Errorf("error reading rsa.PublicKey: %v", err)
-	}
-	pubKey.N = new(big.Int).SetBytes(pubKeyData)
-
-	return pubKey, nil
-}
-
 func xorSessionKeys(key1, key2 []byte) []byte {
-	sessionKey := []byte{}
+	sessionKey := make([]byte, 0)
 	for i := 0; i < len(key1); i++ {
 		sessionKey = append(sessionKey, key1[i]^key2[i])
 	}
