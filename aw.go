08a8e71c36b1ad4e80870dd3b807796533983a23
Jaz Gulati <jaskaran.g15@gmail.com>
2020-08-21 14:59:57 +1000

    aw, aw_test: remove custom tcp server overrides

--- aw.go
+++ aw.go
@@ -94,16 +94,6 @@ func (builder *Builder) WithAddr(addr wire.Address) *Builder {
 	return builder
 }
 
-func (builder *Builder) WithHost(host string) *Builder {
-	builder.trans.TCPServerOpts = builder.trans.TCPServerOpts.WithHost(host)
-	return builder
-}
-
-func (builder *Builder) WithPort(port uint16) *Builder {
-	builder.trans.TCPServerOpts = builder.trans.TCPServerOpts.WithPort(port)
-	return builder
-}
-
 func (builder *Builder) WithTCPClientOptions(opts tcp.ClientOptions) *Builder {
 	builder.trans.TCPClientOpts = opts
 	return builder
