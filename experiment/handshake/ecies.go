1882f8125f5e41f1755d6ebd8ec1efa91910d6f8
bzlw <bzlw@pm.me>
2020-10-30 09:25:29 +1100

    feat: symmetric ecies with comments

--- experiment/handshake/ecies.go
+++ experiment/handshake/ecies.go
@@ -13,199 +13,181 @@ import (
 	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"github.com/renproject/aw/experiment/codec"
 	"github.com/renproject/id"
-	"github.com/renproject/pack"
-	"github.com/renproject/surge"
 )
 
-func ECIESClientHandshake(privKey *id.PrivKey, r *rand.Rand) Handshake {
+const sizeOfSecretKey = 32
+const sizeOfEncryptedSecretKey = 145 // 113-byte encryption header + 32-byte secret key
+
+func ECIES(privKey *id.PrivKey, r *rand.Rand) Handshake {
 	return func(conn net.Conn, enc codec.Encoder, dec codec.Decoder) (codec.Encoder, codec.Decoder, id.Signatory, error) {
+		// Channel for passing errors from the writing goroutine to the reading
+		// goroutine (which has the ability to return the error).
+		errCh := make(chan error, 1)
 
-		serverPubKeyBuf := [keySize * 2]byte{}
-		if _, err := io.ReadFull(conn, serverPubKeyBuf[:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("clieant receiving server Public key: %v", err)
+		// Channel for passing the remote pubkey to the writing goroutine (which
+		// operates in parallel to the reading goroutine).
+		remotePubKeyCh := make(chan id.PubKey, 1)
+		defer close(remotePubKeyCh)
+
+		// Channel for passing the session key to the writing goroutine (which
+		// operates in parallel to the reading goroutine).
+		remoteSecretKeyCh := make(chan []byte, 1)
+		defer close(remoteSecretKeyCh)
+
+		// Generate a local secret key. We do it here, because it is needed by
+		// the writing and reading goroutine.
+		localSecretKey := [sizeOfSecretKey]byte{}
+		if _, err := r.Read(localSecretKey[:]); err != nil {
+			return nil, nil, id.Signatory{}, fmt.Errorf("generate local secret key: %v", err)
 		}
 
-		xAsU256 := pack.NewU256FromInt(big.NewInt(0))
-		yAsU256 := pack.NewU256FromInt(big.NewInt(0))
-		if err := surge.FromBinary(&xAsU256, serverPubKeyBuf[:keySize]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("client converting server Public key X coordinate from bytes: %v", err)
-		}
-		if err := surge.FromBinary(&yAsU256, serverPubKeyBuf[keySize:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("client converting server Public key Y coordinate from bytes: %v", err)
-		}
+		// Begin background goroutine for writing information to the network
+		// connection.
+		go func() {
+			defer close(errCh)
 
-		serverPubKey := id.PubKey{
+			// Write local pubkey so that the remote peer knows how to encrypt
+			// its secret key and send it back to the local peer.
+			localPubKey := privKey.PubKey()
+			xBuf := paddedTo32(localPubKey.X)
+			yBuf := paddedTo32(localPubKey.Y)
+			if _, err := conn.Write(xBuf[:]); err != nil {
+				errCh <- fmt.Errorf("write local pubkey x: %v", err)
+				return
+			}
+			if _, err := conn.Write(yBuf[:]); err != nil {
+				errCh <- fmt.Errorf("write local pubkey y: %v", err)
+				return
+			}
+
+			// Encrypt the local secret key using the remote pubkey and write
+			// it to the remote peer.
+			remotePubKey, ok := <-remotePubKeyCh
+			if !ok {
+				return
+			}
+			importedRemotePubKey := ecies.ImportECDSAPublic((*ecdsa.PublicKey)(&remotePubKey))
+			encryptedLocalSecretKey, err := ecies.Encrypt(r, importedRemotePubKey, localSecretKey[:], nil, nil)
+			if err != nil {
+				errCh <- fmt.Errorf("encrypt local secret key: %v", err)
+				return
+			}
+			if _, err := conn.Write(encryptedLocalSecretKey); err != nil {
+				errCh <- fmt.Errorf("write local secret key: %v", err)
+				return
+			}
+
+			// Encrypted the remote secret key using the remote pubkey and write
+			// it to the remote peer. This allows the remote peer to verify that
+			// the local peer does have access to the previous asserted local
+			// pubkey.
+			remoteSecretKey, ok := <-remoteSecretKeyCh
+			if !ok {
+				return
+			}
+			encryptedRemoteSecretKey, err := ecies.Encrypt(r, importedRemotePubKey, remoteSecretKey, nil, nil)
+			if err != nil {
+				errCh <- fmt.Errorf("encrypt remote secret key: %v", err)
+				return
+			}
+			if _, err := conn.Write(encryptedRemoteSecretKey); err != nil {
+				errCh <- fmt.Errorf("write remote secret key: %v", err)
+				return
+			}
+		}()
+
+		// Read the remote pubkey.
+		remotePubKeyBuf := [64]byte{}
+		if _, err := io.ReadFull(conn, remotePubKeyBuf[:]); err != nil {
+			return nil, nil, id.Signatory{}, fmt.Errorf("read remote pubkey: %v", err)
+		}
+		remotePubKey := id.PubKey{
 			Curve: crypto.S256(),
-			X:     xAsU256.Int(),
-			Y:     yAsU256.Int(),
+			X:     new(big.Int).SetBytes(remotePubKeyBuf[:32]),
+			Y:     new(big.Int).SetBytes(remotePubKeyBuf[32:]),
 		}
+		remotePubKeyCh <- remotePubKey
 
-		clientPubKey := privKey.PubKey()
-		bytesX, err := surge.ToBinary(pack.NewU256FromInt(clientPubKey.X))
+		// Read the encrypted remote secret key, and then decrypt it.
+		encryptedRemoteSecretKey := [sizeOfEncryptedSecretKey]byte{}
+		if _, err := io.ReadFull(conn, encryptedRemoteSecretKey[:]); err != nil {
+			return nil, nil, id.Signatory{}, fmt.Errorf("read remote secret key: %v", err)
+		}
+		remoteSecretKey, err := ecies.ImportECDSA((*ecdsa.PrivateKey)(privKey)).Decrypt(encryptedRemoteSecretKey[:], nil, nil)
 		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client converting own Public key X coordinate to bytes: %v", err)
+			return nil, nil, id.Signatory{}, fmt.Errorf("decrypt remote secret key: %v", err)
 		}
-		bytesY, err := surge.ToBinary(pack.NewU256FromInt(clientPubKey.Y))
+		remoteSecretKeyCh <- remoteSecretKey
+
+		// Read the encrypted local secret back from the remote peer. This
+		// proves to the local peer that the remote peer has access to its
+		// previously asserted pubkey.
+		encryptedLocalSecretKeyCheck := [sizeOfEncryptedSecretKey]byte{}
+		if _, err := io.ReadFull(conn, encryptedLocalSecretKeyCheck[:]); err != nil {
+			return nil, nil, id.Signatory{}, fmt.Errorf("read local secret key: %v", err)
+		}
+		localSecretKeyCheck, err := ecies.ImportECDSA((*ecdsa.PrivateKey)(privKey)).Decrypt(encryptedLocalSecretKeyCheck[:], nil, nil)
 		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client converting own Public key Y coordinate to bytes: %v", err)
+			return nil, nil, id.Signatory{}, fmt.Errorf("decrypt local secret key: %v", err)
 		}
-		if nBytes, err := conn.Write(bytesX); err != nil || nBytes != len(bytesX) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client sending Public key X coordinate to server\n \tError: %v\n\tnumber of bytes written: %v", err, nBytes)
-		}
-		if nBytes, err := conn.Write(bytesY); err != nil || nBytes != len(bytesY) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client sending Public key Y coordinate to server\n\tError: %v\n\tnumber of bytes written: %v", err, nBytes)
+		if bytes.Equal(localSecretKey[:], localSecretKeyCheck[:]) {
+			return nil, nil, id.Signatory{}, fmt.Errorf("check local secret key")
 		}
 
-		eSSecretKeyBuf := [encryptedKeySize]byte{}
-		if _, err := io.ReadFull(conn, eSSecretKeyBuf[:]); err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client receiving server secret key: %v", err)
-		}
-
-		sSecretKey, err := ecies.ImportECDSA((*ecdsa.PrivateKey)(privKey)).Decrypt(eSSecretKeyBuf[:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client decrypting server secret key: %v", err)
-		}
-
-		cSecretKey := [keySize]byte{}
-		if _, err := r.Read(cSecretKey[:]); err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client generating new secret key: %v", err)
-		}
-
-		message := [keySize * 2]byte{}
-		copy(message[:keySize], sSecretKey[:])
-		copy(message[keySize:], cSecretKey[:])
-
-		eMessage, err := ecies.Encrypt(r, ecies.ImportECDSAPublic((*ecdsa.PublicKey)(&serverPubKey)), message[:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client encrypting own secret key: %v", err)
-		}
-
-		if nBytes, err := conn.Write(eMessage); err != nil || nBytes != len(eMessage) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client sending concatenated server secret key and own sercret key\n \tError: %v\n\tnumber of bytes written: %v", err, nBytes)
-		}
-
-		eCSecretKeyBuf := [encryptedKeySize]byte{}
-		if _, err := io.ReadFull(conn, eCSecretKeyBuf[:]); err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client receiving own secret key back from server: %v", err)
-		}
-
-		cSecretKeyEcho, err := ecies.ImportECDSA((*ecdsa.PrivateKey)(privKey)).Decrypt(eCSecretKeyBuf[:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client decrypting own secret key: %v", err)
-		}
-
-		if !bytes.Equal(cSecretKey[:], cSecretKeyEcho[:]) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("client's secret key and echoed secret key from server don't match")
-		}
-
-		sessionKey := [keySize]byte{}
-		for i := 0; i < keySize; i++ {
-			sessionKey[i] = cSecretKey[i] ^ sSecretKey[i]
-		}
-		gcmSession, err := codec.NewGCMSession(sessionKey)
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("establishing gcm session: %v", err)
-		}
-		return codec.GCMEncoder(gcmSession, enc), codec.GCMDecoder(gcmSession, dec), id.NewSignatory(&serverPubKey), nil
-	}
-}
-
-func ECIESServerHandshake(privKey *id.PrivKey, r *rand.Rand) Handshake {
-	return func(conn net.Conn, enc codec.Encoder, dec codec.Decoder) (codec.Encoder, codec.Decoder, id.Signatory, error) {
-
-		serverPubKey := privKey.PubKey()
-		bytesX, err := surge.ToBinary(pack.NewU256FromInt(serverPubKey.X))
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("server converting own Public key X coordinate to bytes: %v", err)
-		}
-		bytesY, err := surge.ToBinary(pack.NewU256FromInt(serverPubKey.Y))
-		if err != nil {
-			return nil, nil, id.Signatory{}, fmt.Errorf("server converting own Public key Y coordinate to bytes: %v", err)
-		}
-		if nBytes, err := conn.Write(bytesX); err != nil || nBytes != len(bytesX) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("server sending Public key X coordinate to server\n \tError: %v\n\tnumber of bytes written: %v", err, nBytes)
-		}
-		if nBytes, err := conn.Write(bytesY); err != nil || nBytes != len(bytesY) {
-			return nil, nil, id.Signatory{}, fmt.Errorf("server sending Public key Y coordinate to server\n\tError: %v\n\tnumber of bytes written: %v", err, nBytes)
-		}
-
-		clientPubKeyBuf := [keySize * 2]byte{}
-		if _, err := io.ReadFull(conn, clientPubKeyBuf[:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server receiving client Public key: %v", err)
-		}
-
-		xAsU256 := pack.NewU256FromInt(big.NewInt(0))
-		yAsU256 := pack.NewU256FromInt(big.NewInt(0))
-		if err := surge.FromBinary(&xAsU256, clientPubKeyBuf[:keySize]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server converting client Public key X coordinate from bytes: %v", err)
-		}
-		if err := surge.FromBinary(&yAsU256, clientPubKeyBuf[keySize:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server converting client Public key Y coordinate from bytes: %v", err)
-		}
-
-		clientPubKey := id.PubKey{
-			Curve: crypto.S256(),
-			X:     xAsU256.Int(),
-			Y:     yAsU256.Int(),
-		}
-
-		sSecretKey := [keySize]byte{}
-		if _, err := r.Read(sSecretKey[:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server generating new secret key: %v", err)
-		}
-		eSSecretKey, err := ecies.Encrypt(r, ecies.ImportECDSAPublic((*ecdsa.PublicKey)(&clientPubKey)), sSecretKey[:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server encrypting secret key: %v", err)
-		}
-
-		if nBytes, err := conn.Write(eSSecretKey); err != nil || nBytes != len(eSSecretKey) {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server sending encrypted secret key to client \n\tError: %v\n\t number of bytes written: %v", err, nBytes)
-		}
-
-		eMessageBuf := [encryptionHeaderSize + keySize*2]byte{}
-		if _, err := io.ReadFull(conn, eMessageBuf[:]); err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server receiving concatenated own secret key and client secret key: %v", err)
-		}
-		message, err := ecies.ImportECDSA((*ecdsa.PrivateKey)(privKey)).Decrypt(eMessageBuf[:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server decrypting concatenated own secret key and client secret key: %v", err)
-		}
-
-		if !bytes.Equal(message[:keySize], sSecretKey[:]) {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server's secret key and echoed secret key from client don't match")
-		}
-
-		eCSecretKey, err := ecies.Encrypt(r, ecies.ImportECDSAPublic((*ecdsa.PublicKey)(&clientPubKey)), message[keySize:], nil, nil)
-		if err != nil {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server encrypting client's secret key: %v", err)
-		}
-
-		if nBytes, err := conn.Write(eCSecretKey); err != nil || nBytes != len(eCSecretKey) {
-			return nil, nil, id.Signatory{},
-				fmt.Errorf("server sending encrypted client secret key back to client \n\tError: %v\n\t number of bytes written: %v", err, nBytes)
-		}
-
-		sessionKey := [keySize]byte{}
-		for i := 0; i < keySize; i++ {
-			sessionKey[i] = message[i] ^ message[keySize+i]
-		}
-		gcmSession, err := codec.NewGCMSession(sessionKey)
-		if err != nil {
+		// Check whether or not that an error happened in the writing goroutine
+		// (and wait for the writing goroutine to end).
+		err, ok := <-errCh
+		if ok {
 			return nil, nil, id.Signatory{}, err
 		}
-		return codec.GCMEncoder(gcmSession, enc), codec.GCMDecoder(gcmSession, dec), id.NewSignatory((*id.PubKey)(&clientPubKey)), nil
+
+		// Build the session key, and use this to build GCM encoders/decoders.
+		sessionKey := [sizeOfSecretKey]byte{}
+		for i := 0; i < sizeOfSecretKey; i++ {
+			sessionKey[i] = localSecretKey[i] ^ remoteSecretKey[i]
+		}
+		gcmSession, err := codec.NewGCMSession(sessionKey)
+		if err != nil {
+			return nil, nil, id.Signatory{}, fmt.Errorf("establish gcm session: %v", err)
+		}
+		return codec.GCMEncoder(gcmSession, enc), codec.GCMDecoder(gcmSession, dec), id.NewSignatory(&remotePubKey), nil
 	}
 }
+
+// paddedTo32 encodes a big integer as a big-endian into a 32-byte array. It
+// will panic if the big integer is more than 32 bytes.
+// Modified from:
+// https://github.com/ethereum/go-ethereum/blob/master/common/math/big.go
+// 17381ecc6695ea9c2d8e5ee0aee5cf70d59a301a
+func paddedTo32(bigint *big.Int) [32]byte {
+	if bigint.BitLen()/8 > 32 {
+		panic(fmt.Sprintf("too big: expected n<32, got n=%v", bigint.BitLen()/8))
+	}
+	ret := [32]byte{}
+	readBits(bigint, ret[:])
+	return ret
+}
+
+// readBits encodes the absolute value of bigint as big-endian bytes. Callers
+// must ensure that buf has enough space. If buf is too short the result will be
+// incomplete.
+// Modified from:
+// https://github.com/ethereum/go-ethereum/blob/master/common/math/big.go
+// 17381ecc6695ea9c2d8e5ee0aee5cf70d59a301a
+func readBits(bigint *big.Int, buf []byte) {
+	i := len(buf)
+	for _, d := range bigint.Bits() {
+		for j := 0; j < wordBytes && i > 0; j++ {
+			i--
+			buf[i] = byte(d)
+			d >>= 8
+		}
+	}
+}
+
+const (
+	// wordBits is the number of bits in a big word.
+	wordBits = 32 << (uint64(^big.Word(0)) >> 63)
+	// wordBytes is the number of bytes in a big word.
+	wordBytes = wordBits / 8
+)
