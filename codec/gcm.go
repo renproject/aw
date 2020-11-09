82d7270727a42e52e3ed0a216b834f59b07acc82
Rahul Ghangas <u6157693@anu.edu.au>
2020-11-09 12:16:46 +1100

    chore: minor style and perf fixes, a bit of cleanup

--- codec/gcm.go
+++ codec/gcm.go
@@ -4,21 +4,22 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"math"
 
 	"github.com/renproject/id"
-	"github.com/renproject/surge"
 )
 
 type gcmNonce struct {
+	// top and bottom together represent the top 32 bits and bottom 64 bits of a 96 bit unsigned integer
 	top       uint32
 	bottom    uint64
 	countDown bool
 }
 
-func (nonce *gcmNonce) change() {
+func (nonce gcmNonce) next() {
 	if nonce.countDown {
 		nonce.pred()
 	} else {
@@ -27,27 +28,28 @@ func (nonce *gcmNonce) change() {
 
 }
 
-func (nonce *gcmNonce) succ() {
+func (nonce gcmNonce) succ() {
 	nonce.bottom += 1
+	// If bottom overflows, increment top by 1
 	if nonce.bottom == 0 {
 		nonce.top += 1
 	}
 }
 
-func (nonce *gcmNonce) pred() {
+func (nonce gcmNonce) pred() {
 	nonce.bottom -= 1
+	// If bottom underflows, decrement top by 1
 	if nonce.bottom == math.MaxUint64 {
 		nonce.top -= 1
 	}
 }
 
 // A GCMSession stores the state of a GCM authenticated/encrypted session. This
-
 // includes the read/write nonces, memory buffers, and the GCM cipher itself.
 type GCMSession struct {
 	gcm        cipher.AEAD
-	readNonce  *gcmNonce
-	writeNonce *gcmNonce
+	readNonce  gcmNonce
+	writeNonce gcmNonce
 }
 
 // NewGCMSession accepts a symmetric secret key and returns a new GCMSession
@@ -64,8 +66,8 @@ func NewGCMSession(key [32]byte, self, remote id.Signatory) (*GCMSession, error)
 
 	gcmSession := &GCMSession{
 		gcm:        gcm,
-		readNonce:  &gcmNonce{},
-		writeNonce: &gcmNonce{},
+		readNonce:  gcmNonce{},
+		writeNonce: gcmNonce{},
 	}
 
 	if bytes.Compare(self[:], remote[:]) < 0 {
@@ -76,24 +78,18 @@ func NewGCMSession(key [32]byte, self, remote id.Signatory) (*GCMSession, error)
 		gcmSession.readNonce.top = math.MaxUint32
 		gcmSession.readNonce.bottom = math.MaxUint64
 		gcmSession.readNonce.countDown = true
-
 	}
 	return gcmSession, nil
 }
 
+// GCMEncoder accepts a GCMSession and an encoder that wraps data encryption
 func GCMEncoder(session *GCMSession, enc Encoder) Encoder {
 	return func(w io.Writer, buf []byte) (int, error) {
-		nonceBufTop, err := surge.ToBinary(session.writeNonce.top)
-		if err != nil {
-			return 0, fmt.Errorf("marshaling nonce to bytes: %v", err)
-		}
-		nonceBufBottom, err := surge.ToBinary(session.writeNonce.bottom)
-		if err != nil {
-			return 0, fmt.Errorf("marshaling nonce to bytes: %v", err)
-		}
-		nonceBuf := append(nonceBufTop, nonceBufBottom...)
-		session.writeNonce.change()
-		encoded := session.gcm.Seal(nil, nonceBuf, buf, nil)
+		nonceBuf := [12]byte{}
+		binary.BigEndian.PutUint32(nonceBuf[:4], session.writeNonce.top)
+		binary.BigEndian.PutUint64(nonceBuf[4:], session.writeNonce.bottom)
+		session.writeNonce.next()
+		encoded := session.gcm.Seal(nil, nonceBuf[:], buf, nil)
 		n, err := enc(w, encoded)
 		if err != nil {
 			return n, fmt.Errorf("Encoded sealed data: %v", err)
@@ -102,23 +98,18 @@ func GCMEncoder(session *GCMSession, enc Encoder) Encoder {
 	}
 }
 
+// GCMDEcoder accepts a GCMSession and a decoder that wraps data decryption
 func GCMDecoder(session *GCMSession, dec Decoder) Decoder {
 	return func(r io.Reader, buf []byte) (int, error) {
 		n, err := dec(r, buf)
 		if err != nil {
 			return n, fmt.Errorf("decoding data: %v", err)
 		}
-		nonceBufTop, err := surge.ToBinary(session.readNonce.top)
-		if err != nil {
-			return 0, fmt.Errorf("marshaling nonce to bytes: %v", err)
-		}
-		nonceBufBottom, err := surge.ToBinary(session.readNonce.bottom)
-		if err != nil {
-			return 0, fmt.Errorf("marshaling nonce to bytes: %v", err)
-		}
-		nonceBuf := append(nonceBufTop, nonceBufBottom...)
-		session.readNonce.change()
-		decrypted, err := session.gcm.Open(nil, nonceBuf, buf[:n], nil)
+		nonceBuf := [12]byte{}
+		binary.BigEndian.PutUint32(nonceBuf[:4], session.readNonce.top)
+		binary.BigEndian.PutUint64(nonceBuf[4:], session.readNonce.bottom)
+		session.readNonce.next()
+		decrypted, err := session.gcm.Open(nil, nonceBuf[:], buf[:n], nil)
 
 		if err != nil {
 			return 0, fmt.Errorf("opening sealed data: %v", err)
