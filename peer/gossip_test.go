e68eb7dceadae580fa547f3754d7146406f6f95f
Rahul Ghangas <u6157693@anu.edu.au>
2021-01-21 02:06:46 +0530

    fix: Fix failing sync test by retrying on failure and move setup to separate file

--- peer/gossip_test.go
+++ peer/gossip_test.go
@@ -5,64 +5,13 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/renproject/aw/channel"
-	"github.com/renproject/aw/dht"
-	"github.com/renproject/aw/handshake"
 	"github.com/renproject/aw/peer"
-	"github.com/renproject/aw/transport"
 	"github.com/renproject/aw/wire"
 	"github.com/renproject/id"
-	"go.uber.org/zap"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
-
-func setup(numPeers int) ([]peer.Options, []*peer.Peer, []dht.Table, []dht.ContentResolver, []*channel.Client, []*transport.Transport) {
-	loggerConfig := zap.NewProductionConfig()
-	loggerConfig.Level.SetLevel(zap.PanicLevel)
-	logger, err := loggerConfig.Build()
-	if err != nil {
-		panic(err)
-	}
-
-	// Init options for all peers.
-	opts := make([]peer.Options, numPeers)
-	for i := range opts {
-		i := i
-		opts[i] = peer.DefaultOptions().WithLogger(logger)
-	}
-
-	peers := make([]*peer.Peer, numPeers)
-	tables := make([]dht.Table, numPeers)
-	contentResolvers := make([]dht.ContentResolver, numPeers)
-	clients := make([]*channel.Client, numPeers)
-	transports := make([]*transport.Transport, numPeers)
-	for i := range peers {
-		self := opts[i].PrivKey.Signatory()
-		h := handshake.Filter(func(id.Signatory) error { return nil }, handshake.ECIES(opts[i].PrivKey))
-		clients[i] = channel.NewClient(
-			channel.DefaultOptions().
-				WithLogger(logger),
-			self)
-		tables[i] = dht.NewInMemTable(self)
-		contentResolvers[i] = dht.NewDoubleCacheContentResolver(dht.DefaultDoubleCacheContentResolverOptions(), nil)
-		transports[i] = transport.New(
-			transport.DefaultOptions().
-				WithLogger(logger).
-				WithClientTimeout(5*time.Second).
-				WithOncePoolOptions(handshake.DefaultOncePoolOptions().WithMinimumExpiryAge(10*time.Second)).
-				WithPort(uint16(3333+i)),
-			self,
-			clients[i],
-			h,
-			tables[i])
-		peers[i] = peer.New(
-			opts[i],
-			transports[i])
-		peers[i].Resolve(context.Background(), contentResolvers[i])
-	}
-	return opts, peers, tables, contentResolvers, clients, transports
-}
 
 var _ = Describe("Gossip", func() {
 	Context("When a node is gossipping with peers", func() {
@@ -101,11 +50,19 @@ var _ = Describe("Gossip", func() {
 				msgHello := fmt.Sprintf("Hi from %v", peers[i].ID().String())
 				contentID := id.NewHash([]byte(msgHello))
 				contentResolvers[i].InsertContent(contentID[:], []byte(msgHello))
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 				defer cancel()
 				peers[i].Gossip(ctx, contentID[:], &peer.DefaultSubnet)
 			}
-			<-time.After(5 * time.Second)
+			<-time.After(4 * time.Second)
+			for i := range peers {
+				for j := range peers {
+					msgHello := fmt.Sprintf("Hi from %v", peers[j].ID().String())
+					contentID := id.NewHash([]byte(msgHello))
+					_, ok := contentResolvers[i].QueryContent(contentID[:])
+					Expect(ok).To(BeTrue())
+				}
+			}
 		})
 	})
 })
--- peer/sync_test.go
+++ peer/sync_test.go
@@ -3,6 +3,7 @@ package peer_test
 import (
 	"context"
 	"fmt"
+	"github.com/renproject/aw/peer"
 	"time"
 
 	"github.com/renproject/aw/wire"
@@ -17,7 +18,15 @@ var _ = Describe("Peer", func() {
 		It("should successfully receive corresponding message", func() {
 
 			n := 2
-			_, peers, tables, contentResolvers, _, _ := setup(n)
+			opts, peers, tables, contentResolvers, _, transports := setup(n)
+
+			for i := range opts {
+				opts[i].SyncerOptions = opts[i].SyncerOptions.WithTimeout(5*time.Second)
+				peers[i] = peer.New(
+					opts[i],
+					transports[i])
+				peers[i].Resolve(context.Background(), contentResolvers[i])
+			}
 
 			tables[0].AddPeer(peers[1].ID(), wire.NewUnsignedAddress(wire.TCP,
 				fmt.Sprintf("%v:%v", "localhost", uint16(3333+1)), uint64(time.Now().UnixNano())))
@@ -36,7 +45,11 @@ var _ = Describe("Peer", func() {
 
 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
+
 			msg, err := peers[1].Sync(ctx, contentID[:], nil)
+			for err != nil {
+				msg, err = peers[1].Sync(ctx, contentID[:], nil)
+			}
 
 			Ω(err).To(BeNil())
 			Ω(msg).To(Equal([]byte(helloMsg)))
