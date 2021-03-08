78a9b53c8b8df68add0c987814e5df2f8dac7a08
Rahul Ghangas <u6157693@anu.edu.au>
2021-03-09 01:37:50 +0530

    test: Add test to check for deletion of sync condition variable after function exit

--- peer/sync_test.go
+++ peer/sync_test.go
@@ -48,11 +48,75 @@ var _ = Describe("Sync", func() {
 
 			msg, err := peers[1].Sync(ctx, contentID[:], nil)
 			for err != nil {
+				select {
+				case <-ctx.Done():
+					panic("Timeout expired before content was synced")
+				default:
+				}
 				msg, err = peers[1].Sync(ctx, contentID[:], nil)
 			}
 
 			Ω(err).To(BeNil())
 			Ω(msg).To(Equal([]byte(helloMsg)))
+		})
+	})
+
+	Context("if a sync request fails", func() {
+		It("the corresponding pending content condition variable should be deleted", func() {
+			n := 2
+			opts, peers, tables, contentResolvers, _, transports := setup(n)
+
+			tables[0].AddPeer(opts[1].PrivKey.Signatory(),
+				wire.NewUnsignedAddress(wire.TCP,
+					fmt.Sprintf("%v:%v", "localhost", uint16(3333+1)), uint64(time.Now().UnixNano())))
+			tables[1].AddPeer(opts[0].PrivKey.Signatory(),
+				wire.NewUnsignedAddress(wire.TCP,
+					fmt.Sprintf("%v:%v", "localhost", uint16(3333)), uint64(time.Now().UnixNano())))
+			ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+			defer cancel()
+			go peers[0].Run(ctx)
+			go func(ctx context.Context) {
+				transports[1].Receive(ctx, func(from id.Signatory, msg wire.Msg) error {
+					time.Sleep(2 * time.Second)
+					if err := peers[1].Syncer().DidReceiveMessage(from, msg); err != nil {
+						return err
+					}
+					if err := peers[1].Gossiper().DidReceiveMessage(from, msg); err != nil {
+						return err
+					}
+					return nil
+				})
+				transports[1].Run(ctx)
+			}(ctx)
+
+			helloMsg := "Hello World!"
+			contentID := id.NewHash([]byte(helloMsg))
+			contentResolvers[0].InsertContent(contentID[:], []byte(helloMsg))
+
+			func() {
+				syncCtx, syncCancel := context.WithTimeout(context.Background(), 2*time.Second)
+				defer syncCancel()
+				msg, err := peers[1].Sync(syncCtx, contentID[:], nil)
+				Expect(msg).To(BeNil())
+				Expect(err).To(Not(BeNil()))
+			}()
+
+			func() {
+				syncCtx, syncCancel := context.WithTimeout(context.Background(), 2*time.Second)
+				defer syncCancel()
+				msg, err := peers[1].Sync(syncCtx, contentID[:], nil)
+				for err != nil {
+					select {
+					case <-syncCtx.Done():
+						panic("Timeout expired before content was synced")
+					default:
+					}
+					msg, err = peers[1].Sync(ctx, contentID[:], nil)
+				}
+				Ω(err).To(BeNil())
+				Ω(msg).To(Equal([]byte(helloMsg)))
+			}()
+
 		})
 	})
 })
